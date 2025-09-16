9c55e264aac625ac7e75118e0a64581c7915bec6
Jeremy <devilskull80@gmail.com>
2025-09-15 23:22:42 -0500

    Adjusted back to 4bit

--- model_setup.py
+++ model_setup.py
@@ -35,19 +35,19 @@ model_id = "microsoft/phi-2"
 adapter_path = "./qlora_phi2/qlora_phi2_best"
 
 
-bnb_config = BitsAndBytesConfig(
-    load_in_8bit=True,
-    llm_int8_threshold=6.0,
-    llm_int8_skip_modules=None,
-    llm_int8_enable_fp32_cpu_offload=False
-)
-
 # bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=True,
-#     bnb_4bit_use_double_quant=True,
-#     bnb_4bit_quant_type="nf4",
-#     bnb_4bit_compute_dtype="float16",
+#     load_in_8bit=True,
+#     llm_int8_threshold=6.0,
+#     llm_int8_skip_modules=None,
+#     llm_int8_enable_fp32_cpu_offload=False
 # )
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype="float16",
+)
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 tokenizer.pad_token = tokenizer.eos_token
